2d39083bdd9bfaac334028b6f190b225515667ac
Ami Tavory <atavory@gmail.com>
2017-08-05 03:24:51 +0300

    no ddt init

--- test/_test.py
+++ test/_test.py
@@ -60,6 +60,43 @@ def _load_digits():
     return digits, features
 
 
+
+if False:
+    class _EstimatorTest(unittest.TestCase):
+        pass
+
+
+    def _generate_nb_tests():
+
+        def test(self):
+            cmd = 'jupyter-nbconvert --to notebook --execute %s --output %s --ExecutePreprocessor.timeout=7200' % (name, name)
+
+            try:
+                self.assertEqual(os.system(cmd), 0)
+            except Exception as exc:
+                print(cmd, exc)
+                # Python2.7 fails on travis, for some reason
+                if six.PY3:
+                    raise
+        return test
+
+
+    nb_f_names = list(glob(os.path.join(_this_dir, '../examples/*.ipynb')))
+    nb_f_names = [n for n in nb_f_names if '.nbconvert.' not in n]
+    for n in nb_f_names:
+        with (open(n, encoding='utf-8') if six.PY3 else open(n)) as f:
+            cnt = json.loads(f.read(), encoding='utf-8')
+        metadata = cnt['metadata']
+        if 'ibex_test_level' not in metadata:
+            raise KeyError('ibex_test_level missing from metadata of ' + n)
+        if _level < int(metadata['ibex_test_level']):
+            continue
+
+        test_name = 'test_' + os.path.splitext(os.path.split(n)[1])[0]
+        test = _generate_nb_tests(n)
+        setattr(_NBsTest, test_name, test)
+
+
 class _ConceptsTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
