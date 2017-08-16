8dfe1a2a105a561087cb95f92e563110e690c709
Ami Tavory <atavory@gmail.com>
2017-08-16 21:32:30 +0300

    forgot to add ensemble to ibex.sklearn

---
+++ ibex/sklearn/_ensemble.py
@@ -0,0 +1,88 @@
+from __future__ import absolute_import
+
+
+import functools
+import inspect
+
+import pandas as pd
+from sklearn import base
+
+from .._adapter import frame_with_post_ops
+from ._utils import get_matching_estimators
+
+
+_extra_doc = """
+
+.. tip::
+
+    Transformers in this module label their columns as ``comp_0``, ``comp_1``, and so on.
+
+    Example
+
+        >>> import pandas as pd
+        >>> import numpy as np
+        >>> from ibex.sklearn import datasets
+        >>> from ibex.sklearn.decomposition import PCA as PDPCA
+
+        >>> iris = datasets.load_iris()
+        >>> features = iris['feature_names']
+        >>> iris = pd.DataFrame(
+        ...     np.c_[iris['data'], iris['target']],
+        ...     columns=features+['class'])
+
+        >>> iris[features]
+        sepal length (cm)  sepal width (cm)  petal length (cm)  petal width (cm)
+        0                5.1               3.5                1.4               0.2
+        1                4.9               3.0                1.4               0.2
+        2                4.7               3.2                1.3               0.2
+        3                4.6               3.1                1.5               0.2
+        4                5.0               3.6                1.4               0.2
+        ...
+
+        >>> PDPCA(n_components=2).fit(iris[features], iris['class']).transform(iris[features])
+            comp_0    comp_1
+        0   -2.684207  0.326607
+        1   -2.715391 -0.169557
+        2   -2.889820 -0.137346
+        3   -2.746437 -0.311124
+        4   -2.728593  0.333925
+        ...
+
+"""
+
+
+def _from_pickle(est, params):
+    est = frame(est)
+
+    _update_est(est)
+
+    return est(**params)
+
+
+def _wrap_getattr(fn):
+    @functools.wraps(fn)
+    def wrapped(self, name, *args, **kwargs):
+        ret = fn(self, name, *args, **kwargs)
+        if name == 'feature_importances_':
+            return pd.Series(ret, index=self.x_columns)
+        return ret
+    return wrapped
+
+
+def _update_est(est):
+    est.__getattribute__ = _wrap_getattr(est.__getattribute__)
+    est.__reduce__ = lambda self: (_from_pickle, (inspect.getmro(est)[1], self.get_params(deep=True), ))
+
+
+def update_module(module):
+    module.__doc__ += _extra_doc
+
+    for est in get_matching_estimators(module, base.BaseEstimator):
+        est = frame_with_post_ops(est, [])
+        _update_est(est)
+        setattr(module, est.__name__, est)
+
+
+
+
+
