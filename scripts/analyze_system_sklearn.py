60070d34ad6f360f581b0f189a8cb85e448cc9c4
Ami Tavory <atavory@fb.com>
2017-09-01 08:26:57 -0700

    dendrogram

--- scripts/analyze_system_sklearn.py
+++ scripts/analyze_system_sklearn.py
@@ -8,7 +8,9 @@ from sklearn import base
 
 
 m_names = set()
+js_c_names = [{'id': 'sklearn', 'value': ''}]
 for mod_name in sklearn.__all__:
+    js_c_names += [{'id': 'sklearn.' + mod_name, 'value': ''}]
     import_str = 'from sklearn import %s as _orig' % mod_name
     exec(import_str)
     for name in dir(_orig):
@@ -18,6 +20,7 @@ for mod_name in sklearn.__all__:
                 continue
         except TypeError:
             continue
+        js_c_names += [{'id': 'sklearn.' + mod_name + '.' + name, 'value': '1'}]
         for m_name in c.__dict__:
             if m_name.startswith('_'):
                 continue
@@ -36,5 +39,10 @@ print('\n'.join(sorted(sklearn.__all__)))
 
 print('\n\nmethods:\n\n')
 print('\n'.join(sorted(m_names)))
+
+print('\n\js_c_names:\n\n')
+print('[')
+print(',\n'.join([('\t' + str(d)) for d in js_c_names]))
+print(']')
 
 
