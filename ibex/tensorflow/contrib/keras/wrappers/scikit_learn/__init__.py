87888b475a698dcc25fd15e021874ed606035449
Ami Tavory <atavory@fb.com>
2017-09-23 02:35:07 +0300

    work already

--- ibex/tensorflow/contrib/keras/wrappers/scikit_learn/__init__.py
+++ ibex/tensorflow/contrib/keras/wrappers/scikit_learn/__init__.py
@@ -94,7 +94,7 @@ class KerasClassifier(KerasEstimator, base.ClassifierMixin):
         ...     classes=iris['class'].unique(),
         ...     verbose=0)
         >>> clf.fit(iris[features], iris['class'])
-        Adapter[KerasClassifier](verbose=0,build_fn=<function _build_classifier_nn at ...>,classes=[ 0.  1.  2.]...)
+        Adapter[KerasClassifier](...classes=[ 0.  1.  2.]...)
         >>> clf.history_
         <tensorflow.contrib.keras.python.keras.callbacks.History object at ...>
         >>> clf.fit(iris[features], iris['class']).predict(iris[features])
